48f86b706f84c2aa8f98166bc53314f4ab766064
Sergio Enrique Vargas <sergioenrique@me.com>
2015-11-04 22:42:01 -0600

    Proyecto para algunas apis

---
+++ Conekta.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Valerio;
+
+//srequire_once dirname(__FILE__).'/../../vendor/conekta/conekta-php/lib/Conekta.php';
+
+/**
+* 
+*/
+class Conekta
+{
+	protected $id;
+
+	function __construct()
+	{
+		# code...
+	}
+}
